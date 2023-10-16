110b0b65f2d419c50162e8bb4424f3b8641d56a0
AbhiVikrant <abhishekvikrantdba123@gmail.com>
2023-10-16 13:42:19 +0530

    Added handleChange Event for Inc,Dec and Reset

--- Counter/countus/src/App.js
+++ Counter/countus/src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 const App = () => {
+  let [count, setCount] = useState(0);
+  function handleClickIncrease() {
+          setCount(count + 1)
+  }
+  const handleClickDecrease=() => {
+    setCount(count - 0.5);
+  }
+  const handleClickReset=() => {
+    setCount(0);
+  }
+
   return (
-    <>
-      <div className="appMain">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident, quo adipisci nobis reiciendis, architecto eum recusandae tenetur eveniet eligendi magnam dolor? Natus velit ea praesentium suscipit quam, minus accusamus?</p>
+    <div>
+      <h1>Count Me</h1>
+
+        <p>
+         Current count of mine be
+          <b> {count} </b>.
+      </p>
+      <div className="buttonClass">
+      <button type="button" onClick={handleClickDecrease}>Decrease</button>
+      <button type="button" onClick={handleClickReset}>Reset</button>
+      <button onClick={handleClickIncrease}>Increase</button>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 export default App;
