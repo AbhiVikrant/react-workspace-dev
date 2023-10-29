0a35e9b314354b90059063b326e12d47de33f5b9
AbhiVikrant <abhishekvikrantdba123@gmail.com>
2023-10-30 01:23:20 +0530

    Added Reach Me props

---
+++ react-props/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reach me</h1>
+      {/* Code here */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-props/src/components/ListItem.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+// Complete this Component
+const ListItem = () => {
+  return (
+    <div
+      className="ListItem"
+      style={{
+        height: 30
+      }}
+    >
+      <img src="" alt="" />
+      <a href=""></a>
+    </div>
+  );
+};
+
+export default ListItem;
